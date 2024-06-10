3a7a0f94f20a8f7fe6198747add05a41a5eba3e0
basliel <baslielgetu97@gmail.com>
2024-06-10 21:51:22 +0300

    hello

---
+++ app/src/main/java/com/healthcareapp/HomeScreen.java
@@ -0,0 +1,17 @@
+package com.healthcareapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeScreen extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+
+
+
+
+    }
+}
